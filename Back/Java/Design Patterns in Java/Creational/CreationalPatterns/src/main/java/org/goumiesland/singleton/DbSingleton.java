7c793d4d11fe9fb78e4ad231c7c68cdf51b3fb31
Romy Alula <goumiesone.dev@gmail.com>
2020-03-13 15:50:07 +0100

    Lazyly load Singleton

--- Back/Java/Design Patterns in Java/Creational/CreationalPatterns/src/main/java/org/goumiesland/singleton/DbSingleton.java
+++ Back/Java/Design Patterns in Java/Creational/CreationalPatterns/src/main/java/org/goumiesland/singleton/DbSingleton.java
@@ -2,12 +2,15 @@ package org.goumiesland.singleton;
 
 public class DbSingleton {
 
-    private static DbSingleton instance = new DbSingleton();
+    private static DbSingleton instance = null;
 
-    private DbSingleton() {
-    }
+    private DbSingleton() {}
 
     public static DbSingleton getInstance() {
+        // Not thread safe but substantial performance improvement
+        if(instance == null)
+            instance = new DbSingleton();
+
         return instance;
     }
 }
