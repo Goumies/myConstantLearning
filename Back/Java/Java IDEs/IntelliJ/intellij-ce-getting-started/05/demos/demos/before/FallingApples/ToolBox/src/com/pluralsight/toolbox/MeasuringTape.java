145584ead57ced44ae7b8fbc76c81302b906af7c
Romy Alula <goumiesone.dev@gmail.com>
2020-03-23 19:27:11 +0100

    Add Getting started with IntelliJ CE

---
+++ Back/Java/Java IDEs/IntelliJ/intellij-ce-getting-started/05/demos/demos/before/FallingApples/ToolBox/src/com/pluralsight/toolbox/MeasuringTape.java
@@ -0,0 +1,17 @@
+package com.pluralsight.toolbox;
+
+import java.util.Random;
+
+public class MeasuringTape {
+
+
+    // TODO: Implement a Measuring Tape that actually measures distances
+    public double getDistance(Tree tree, IFruit fruit) {
+
+        Random random = new Random();
+
+        return random.nextDouble() * fruit.getRollMultiplier();
+
+    }
+
+}
